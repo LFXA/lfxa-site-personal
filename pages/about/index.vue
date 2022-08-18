59dc61616277e1c820037053de4f0a5367b5d4bf
Lucas Felix Aquino <ext.lucas.aquino@farfetch.com>
2022-08-18 22:57:02 +0100

    add about page and blog page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,20 +2,59 @@
 <div>
    <header class="shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold leading-tight text-copy-primary">About</h1>
+          <h1 class="text-3xl font-bold leading-tight text-copy-primary">{{ $t("about.title") }}</h1>
         </div>
       </header>
       <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <!-- Replace with your content -->
-          <div class="px-4 py-6 sm:px-0">
-            <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+        <div class="flex flex-col md:flex-row max-w-7xl mx-auto py-6 px-6 md:px-8">
+            <img class="w-48 h-48 mb-8 rounded-full mx-auto" src="../../assets/perfil_lfxa.jpg" alt=""/>
+            <div class="lg:text-justify text-left text-2xl leading-snug md:pl-12 text-copy-secondary">
+              <p>
+                {{ $t("about.text1") }}
+                <span class="italic font-extrabold">Lucas Felix Aquino</span>,
+                {{ $t("about.text2") }} {{ calculaIdade(idade) }}
+                {{ $t("about.text3") }} <nuxt-link :to="$t('about.link')">{{ $t("about.textLink") }}</nuxt-link>
+              </p>
+              <p class="font-extrabold">
+                <br />
+                {{ $t("about.text4") }}
+              </p>
+              <p><br />{{ $t("about.text5") }}</p>
+            </div>       
+               
           </div>
-          <!-- /End replace -->
-        </div>
       </main>
 </div>
 </template>
 <script>
+export default {
 
+  data() {
+    return {
+      idade: new Date(1998, 6, 7, 11, 10),
+    };
+  },
+  methods: {
+    calculaIdade(dataNasc) {
+      let dataAtual = new Date();
+      let anoAtual = dataAtual.getFullYear();
+      let diaNasc = dataNasc.getDate();
+      let mesNasc = dataNasc.getMonth() + 1;
+      let anoNasc = dataNasc.getFullYear();
+      let idade = anoAtual - anoNasc;
+      let mesAtual = dataAtual.getMonth() + 1;
+
+      if (mesAtual < mesNasc) {
+        idade--;
+      } else {
+        if (mesAtual == mesNasc) {
+          if (new Date().getDate() < diaNasc) {
+            idade--;
+          }
+        }
+      }
+      return idade;
+    },
+  },
+};
 </script>
